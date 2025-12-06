47d85f8a4d53369dc84e8b8af1e38c6ac754ede5
Hamza <hamzachebbah9999@gmail.com>
2025-12-06 14:58:49 +0100

    added js

---
+++ exercices/Javascript/exercice1.js
@@ -0,0 +1,24 @@
+//--------------------------------------------------- EXO 1 ---------------------------------------------------
+
+//- Write a single line comment which says : comments can make code readable
+
+//- Write a multiline comment which says : comments can make code readable, easy to reuse and informative
+
+//- Create a variable.js file and declare variables and assign string, boolean, undefined and null data types
+
+
+
+//- in variable.js file and use the JavaScript typeof operator to check different data types.
+//  Check the data type of each variable
+
+//- Declare four variables without assigning values
+
+//- Declare variables to store your first name, last name, isOnline, country and age(number) in multiple lines
+
+
+//- Declare variables to store your first name, last name, marital status, country and age(number) in a single line
+
+// - Declare two variables myAge and yourAge and assign them initial values and log to the browser console like this :
+//   I am 25 years old.
+//   You are 30 years old.
+
---
+++ workshops/js-workshop/index.js
@@ -0,0 +1,28 @@
+console.log("hello world");
+
+
+// var formation = "JS FULLSTACK"
+// const Username = "hamza"
+// let ecole = "gmc"
+
+// // Username = "islem"
+// formation = "AI"
+
+// console.log(formation, Username, ecole)
+
+var age = 14
+if (age > 12) {
+  var username = "hamza"
+  console.log("you are allowed to go to cinema", username)
+}
+
+console.log("name", username)
+
+var isOpen = true
+const age = "22"
+const username = 'hamza'
+let job
+
+
+
+
